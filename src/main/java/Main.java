f480c8d0eb699d685c47858bf1ace8acb2967788
Andrey M <Moskovets.And@gmail.com>
2022-10-27 11:07:22 +0900

    added new branch-MAX, added new method find max on interval

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,53 +1,75 @@
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
+import java.util.concurrent.*;
 
 
 public class Main {
 
-    public static final int SIZE = 25;
+    private static final int SIZE = 500;
 
     public static void main(String[] args) throws InterruptedException {
 
-        List<Thread> threads = new ArrayList<>();
+        //расчет оптимального пула
+        int poolSize = Runtime.getRuntime().availableProcessors() + 1;
+        ExecutorService threadPool = Executors.newFixedThreadPool(poolSize);
+        List<Future<Integer>> futures = new ArrayList<>();
+
         long startTs = System.currentTimeMillis(); // start time
 
         for (int i = 0; i < SIZE; i++) {
-            Thread thread = new Thread(() -> calcInterval(generateText("aab", 30_000)));
-            thread.setName("Thread - " + i);
-            thread.start();
-            threads.add(thread);
-
+            futures.add(threadPool.submit(new CaclMaxInterval(generateText("aab", 30_000))));
         }
 
-        for (Thread thread : threads) {
-            thread.join();
+        int max = 0;
+        for (Future<Integer> future : futures) {
+            try {
+                max = future.get();
+            } catch (ExecutionException e) {
+                throw new RuntimeException(e);
+            }
         }
 
         long endTs = System.currentTimeMillis(); // end time
         System.out.println("Time: " + (endTs - startTs) + "ms");
+
+        System.out.println("Максимальный интервал значений: " + max);
+        threadPool.shutdown();
     }
 
-    public static void calcInterval(String text) {
-        int maxSize = 0;
-        for (int i = 0; i < SIZE; i++) {
-            for (int j = 0; j < SIZE; j++) {
-                if (i >= j) {
-                    continue;
-                }
-                boolean bFound = false;
-                for (int k = i; k < j; k++) {
-                    if (text.charAt(k) == 'b') {
-                        bFound = true;
-                        break;
+    public static class CaclMaxInterval implements Callable<Integer> {
+        private final String text;
+        private static int max;
+
+        public CaclMaxInterval(String text) {
+            this.text = text;
+        }
+
+        @Override
+        public Integer call() throws Exception {
+            int maxSize = 0;
+            for (int i = 0; i < SIZE; i++) {
+                for (int j = 0; j < SIZE; j++) {
+                    if (i >= j) {
+                        continue;
+                    }
+                    boolean bFound = false;
+                    for (int k = i; k < j; k++) {
+                        if (text.charAt(k) == 'b') {
+                            bFound = true;
+                            break;
+                        }
+                    }
+                    if (!bFound && maxSize < j - i) {
+                        maxSize = j - i;
                     }
                 }
-                if (!bFound && maxSize < j - i) {
-                    maxSize = j - i;
-                }
+                if (max < maxSize)
+                    max = maxSize;
             }
+            System.out.println(text.substring(0, 100) + " -> " + maxSize);
+            return max;
         }
-        System.out.println(text.substring(0, 100) + " -> " + maxSize);
     }
 
     public static String generateText(String letters, int length) {
