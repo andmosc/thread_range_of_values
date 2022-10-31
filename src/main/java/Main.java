d3b33aecde10a43373855da22e2a54b67a261765
Andrey M <Moskovets.And@gmail.com>
2022-10-31 18:52:38 +0900

    editing task1

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,6 +6,7 @@ import java.util.Random;
 public class Main {
 
     public static final int SIZE = 25;
+    public static final int SIZE_STRING = 30_000;
 
     public static void main(String[] args) throws InterruptedException {
 
@@ -13,11 +14,10 @@ public class Main {
         long startTs = System.currentTimeMillis(); // start time
 
         for (int i = 0; i < SIZE; i++) {
-            Thread thread = new Thread(() -> calcInterval(generateText("aab", 30_000)));
+            Thread thread = new Thread(() -> calcInterval(generateText("aab", SIZE_STRING)));
             thread.setName("Thread - " + i);
             thread.start();
             threads.add(thread);
-
         }
 
         for (Thread thread : threads) {
@@ -30,8 +30,8 @@ public class Main {
 
     public static void calcInterval(String text) {
         int maxSize = 0;
-        for (int i = 0; i < SIZE; i++) {
-            for (int j = 0; j < SIZE; j++) {
+        for (int i = 0; i < SIZE_STRING; i++) {
+            for (int j = 0; j < SIZE_STRING; j++) {
                 if (i >= j) {
                     continue;
                 }
